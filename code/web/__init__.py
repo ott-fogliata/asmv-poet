15473cf1f1932c297128e8b5e03ff1fda5c23217
Ottavio Fogliata <ott.fogliata@gmail.com>
2018-08-23 08:53:51 +0200

    flaskr microframework + project structure; (AI RNN) porting from old tensorflow version to latest

--- code/web/__init__.py
+++ code/web/__init__.py
@@ -2,12 +2,15 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'web.sqlite'),
     )
 
     if test_config is None:
@@ -27,6 +30,5 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-
 
     return app
